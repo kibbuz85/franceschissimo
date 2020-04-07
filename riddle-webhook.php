20e99a042ea69e2066da227252339a81425717e3
kibbuz85 <63310202+kibbuz85@users.noreply.github.com>
2020-04-07 19:40:53 +0200

    Update riddle-webhook.php

--- riddle-webhook.php
+++ riddle-webhook.php
@@ -1,6 +1,14 @@
 <?php
 
-require dirname(__FILE__) . '/src/init.php';
-
-$handler = new Riddle\Core\RiddleLeaderboardHandler();
-echo $handler->start();
+$config = [
+    /**
+     * Secret of your riddle extension.
+     * DON'T share this secret. You only have to enter this secret along with the rest of the URL as a riddle custom landing page.
+     * example: your-landing-page.com?secret=[YOUR SECRET]
+     * 
+     * It's recommended to generate a new secret. Use this link to get one in 10 seconds:
+     * https://www.random.org/strings/?num=1&len=16&digits=on&upperalpha=on&loweralpha=on&unique=on&format=html&rnd=new
+     */
+    'secret' => 'iyeiHo5MJ857vHGe',
+];
+© 2020 GitHub, Inc.
